c29224774f99ff8d26e15a199597f357f2e7e7d6
Eugen Geisler <etensil@gmail.com>
2013-11-27 10:34:00 +0100

    change plugin loading

--- app/assets/javascripts/jquery-fileupload/index.js
+++ app/assets/javascripts/jquery-fileupload/index.js
@@ -1,7 +1,9 @@
 //=require jquery-fileupload/vendor/jquery.ui.widget
-//=require jquery-fileupload/vendor/load-image/load-image
+//=require jquery-fileupload/vendor/load-image/index
 //=require jquery-fileupload/vendor/canvas-to-blob
 //=require jquery-fileupload/vendor/tmpl
 //=require jquery-fileupload/jquery.iframe-transport
 //=require jquery-fileupload/jquery.fileupload
+//=require jquery-fileupload/jquery.fileupload-process
+//=require jquery-fileupload/jquery.fileupload-image
 //=require jquery-fileupload/jquery.fileupload-ui
